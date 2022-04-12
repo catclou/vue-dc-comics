7570ba6b6e4475f29c1a61de43c163c33a4664dd
catclou <caterina.moccia@gmail.com>
2022-04-12 16:05:07 +0200

    blank canvas

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,18 @@
+<template>
+    <header>
+        <h2>prova</h2>
+    </header>
+</template>
+
+<script>
+
+
+export default {
+    name: 'HeaderComp',
+    
+}
+</script>
+
+<style scoped>
+
+</style>
