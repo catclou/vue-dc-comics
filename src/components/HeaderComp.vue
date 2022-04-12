050b69a342454d02d850e9f89b5b15bcadbfd262
catclou <caterina.moccia@gmail.com>
2022-04-12 16:31:32 +0200

    sketched header+navbar

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,18 +1,27 @@
 <template>
     <header>
-        <h2>prova</h2>
+        <img src="../assets/img/dc-logo.png" alt="DC logo">
+        <MainNavbarComp></MainNavbarComp>
     </header>
 </template>
 
 <script>
 
+import MainNavbarComp from './MainNavbarComp.vue'
 
 export default {
     name: 'HeaderComp',
-    
+    components: {
+    MainNavbarComp
+    }
 }
 </script>
 
 <style scoped>
-
+    header {
+        display: flex;
+        justify-content: space-around;
+        border: 2px solid skyblue;
+        padding: 1rem;
+    }
 </style>
---
+++ src/components/MainNavbarComp.vue
@@ -0,0 +1,48 @@
+<template>
+    <nav>
+        <ul>
+            <li>characters</li>
+            <li>comics</li>
+            <li>movies</li>
+            <li>tv</li>
+            <li>games</li>
+            <li>collectibles</li>
+            <li>videos</li>
+            <li>fans</li>
+            <li>news</li>
+            <li>shop</li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+
+
+export default {
+    name: 'MainNavbarComp',
+}
+</script>
+
+<style scoped>
+    ul {
+        display: flex;
+        align-items: center;
+    }
+
+    li {
+        padding: 1rem;
+        list-style: none;
+        align-self: center;
+        border: 1px solid yellowgreen;
+        text-transform: uppercase;
+        font-weight: bold;
+        padding-bottom: 2rem;
+        /* aggiungere border-bottom e color blue all'hover */
+        /* border-bottom: 2px solid blue; */
+    }
+
+    nav {
+        border: 1px dotted green;
+        
+    }
+</style>
